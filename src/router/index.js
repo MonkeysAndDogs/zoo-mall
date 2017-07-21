c70890ec9da58014012851520ee9f96a0bfd8ec1
曲士界 <qusj@tydic.com>
2017-07-21 21:00:57 +0800

    动态路由demo

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Student from '../views/Student.vue'
 
 Vue.use(Router)
 
 export default new Router({
+  mode:'hash', /*default*/
   routes: [
     {
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/class/:classId/student/:studentNo',
+      name: 'Student',
+      component: Student
     }
   ]
 })
---
+++ src/views/Student.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <P>Student Info</p>
+    <label>class:</label><span>{{$route.params.classId}}</span><br>
+    <label>studentNo:</label><span>{{$route.params.studentNo}}</span>
+  </div>
+</template>
+<style>
+
+</style>
+<script>
+  export default {
+    data(){
+
+    },
+    components:[
+
+    ],
+    methods:{
+
+    }
+  }
+</script>
