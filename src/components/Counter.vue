2e14d44bd3fda38b867d6bf092b55be073b0c204
曲士界 <qusj@tydic.com>
2017-07-21 15:01:01 +0800

    firt code commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <button>+</button>
+    <button>-</button>
+  </div>
+</template>
+<style>
+
+</style>
+<script>
+  export default{
+      data(){
+          return {
+              msg: 'hello vue'
+          }
+      },
+      methods:{
+
+      }
+  }
+
+</script>
