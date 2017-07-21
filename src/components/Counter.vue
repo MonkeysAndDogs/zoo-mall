b233431240fbddee99e522e0997c9840bbe968f9
曲士界 <qusj@tydic.com>
2017-07-21 20:10:04 +0800

    add Counter.vue

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,22 +1,38 @@
 <template>
   <div>
-    <button>+</button>
-    <button>-</button>
+    <button v-on:click="increment">+</button>
+    <button @click="decrement">-</button>
+    <p><span>{{num2}}</span></p>
   </div>
 </template>
 <style>
 
 </style>
 <script>
-  export default{
-      data(){
-          return {
-              msg: 'hello vue'
-          }
-      },
-      methods:{
-
+  var son = 'zzh'
+  export default {
+    props:["num2"],
+    data() {
+      return {
+        num2: 16,
+        msg: 'hello vue'
       }
+    },
+    abc:123,
+    methods: {
+      increment() {
+//        console.log(this.$parent)
+//        console.log(this.$props)
+//        console.log(this)
+//        console.l og(this.data())
+//        this.num++;
+        this.$emit("incre")
+      },
+      decrement() {
+//        this.num--;
+        this.$emit("decre")
+      }
+    }
   }
 
 </script>
--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <counter v-bind:num2="num" v-on:incre="increment" v-on:decre="decrement"></counter>
+    <p>parent:{{num}}</p>
+    <!--
     <h2>Essential Links</h2>
     <ul>
       <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
@@ -16,38 +19,53 @@
       <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
       <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    </ul>-->
+
   </div>
 </template>
 
 <script>
-export default {
-  name: 'hello',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  import Counter from './Counter'
+
+  export default {
+    name: 'hello',
+    data() {
+      return {
+        num:8,
+        msg: 'Welcome to Your Vue.js App'
+      }
+    },
+    components: {
+      Counter
+    },
+    methods:{
+      increment(){
+        this.num++;
+      },
+      decrement(){
+        this.num--;
+      }
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
+  h1, h2 {
+    font-weight: normal;
+  }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
 
-a {
-  color: #42b983;
-}
+  a {
+    color: #42b983;
+  }
 </style>
